4f81b9579b18f9858aede69bb25980fda2a446ab
vempatisriram6-dev <vempatisriram6@gmail.com>
2026-01-06 14:20:00 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import os
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)  
 
-APP_ENV = os.getenv("ENV", "dev")
-
-@app.route("/health")
-def health():
-    return jsonify(status="OK", env=APP_ENV), 200
-
+# Root route
 @app.route("/")
 def home():
-    return "Backend is running"
+    return jsonify({
+        "status": "Backend is running"
+    })
+
+# Health check route
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "OK",
+        "env": os.getenv("ENV", "dev")
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
