af21285f525a3d0540d62a23063c858c35293b07
vempatisriram6-dev <vempatisriram6@gmail.com>
2026-01-06 14:36:27 +0530

    Update health_check.sh

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -4,19 +4,19 @@ URL="http://localhost:80"
 RETRIES=5
 SLEEP=3
 
-echo "Ì¥ç Checking container health at $URL"
+echo " Checking container health at $URL"
 
 for i in $(seq 1 $RETRIES); do
   if curl -fs $URL > /dev/null; then
-    echo "‚úÖ Frontend is healthy"
+    echo " Frontend is healthy"
     exit 0
   fi
 
-  echo "‚è≥ Attempt $i failed. Retrying in $SLEEP seconds..."
+  echo " Attempt $i failed. Retrying in $SLEEP seconds..."
   sleep $SLEEP
 done
 
-echo "‚ùå Frontend health check failed"
+echo " Frontend health check failed"
 exit 1
 
 
