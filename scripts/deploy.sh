bbf9aaa09fb10ff53ad19f3decf7320437ccde31
vempatisriram6-dev <vempatisriram6@gmail.com>
2026-01-06 14:35:51 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ APP_NAME=frontend
 BACKUP_IMAGE_FILE=/tmp/last_image.txt
 
 if [ ! -f $BACKUP_IMAGE_FILE ]; then
-  echo "❌ No previous image found to rollback"
+  echo " No previous image found to rollback"
   exit 1
 fi
 
 PREVIOUS_IMAGE=$(cat $BACKUP_IMAGE_FILE)
 
-echo "⏪ Rolling back to $PREVIOUS_IMAGE"
+echo " Rolling back to $PREVIOUS_IMAGE"
 
 docker stop $APP_NAME || true
 docker rm $APP_NAME || true
@@ -20,7 +20,7 @@ docker run -d \
   -p 80:80 \
   $PREVIOUS_IMAGE
 
-echo "✅ Rollback completed"
+echo " Rollback completed"
 
 
 
